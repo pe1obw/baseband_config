b85e95cd71903e45ec80b0b5d39b25e68ac36d71
Werner Damman <56766970+pe1obw@users.noreply.github.com>
2024-04-08 10:38:19 +0200

    CHG: Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='baseband_config',
-    version='1.0',
+    version='0.9',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
@@ -11,13 +11,9 @@ setup(
     },
     description='A package for controlling the Baseband device using an USB-i2c interface.',
     author='PE1OBW',
-    author_email='werner@pe1obw.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
