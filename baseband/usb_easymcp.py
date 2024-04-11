a3ea1c98cbdeb6a5d508cd03e3b3d03cb281980e
Werner <werner@pe1obw.com>
2024-04-11 20:08:59 +0200

    FIX: Increased timeout to 500ms for MCP2221

--- baseband/usb_easymcp.py
+++ baseband/usb_easymcp.py
@@ -1,6 +1,7 @@
 """
 USB interface for the MCP2221A USB to I2C bridge.
 """
+import time
 from typing import Optional
 import EasyMCP2221
 
@@ -12,14 +13,14 @@ class UsbEasyMcp:
         self.mcp.I2C_speed(400000)
 
     def write(self, data: bytes):
-        self.mcp.I2C_write(self.SLAVE_ADDR, data, timeout_ms=100)
+        self.mcp.I2C_write(self.SLAVE_ADDR, data, timeout_ms=500)
 
     def read(self, length: int) -> bytes:
-        return bytes(self.mcp.I2C_read(self.SLAVE_ADDR, length, timeout_ms=100))
+        return bytes(self.mcp.I2C_read(self.SLAVE_ADDR, length, timeout_ms=500))
 
     def exchange(self, data: bytes, length: int) -> bytes:
-        self.mcp.I2C_write(self.SLAVE_ADDR, data, kind='nonstop', timeout_ms=100)
-        result = self.mcp.I2C_read(self.SLAVE_ADDR, length, kind='restart', timeout_ms=100)
+        self.mcp.I2C_write(self.SLAVE_ADDR, data, kind='nonstop', timeout_ms=500)
+        result = self.mcp.I2C_read(self.SLAVE_ADDR, length, kind='restart', timeout_ms=500)
         return bytes(result)
 
     def pulse_gpio(self, gpio_nr: int, seconds: float):
